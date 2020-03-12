8471d516986aa52f35974fec5ed0917d2af396c4
RazzSunuwar <razzkoinch5@gmail.com>
2020-03-12 10:01:15 +0545

    camelCased Property Names

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// Example: Insert an object with the styling information:
+// Example: Use backgroundColor instead of background-color:
 
 // import './index.css';
 // import App from './App';
@@ -9,14 +9,10 @@ class MyHeader extends React.Component {
     render(){
         return (
             <div>
-                <h1 style= {{color : "#76FF03"}}>Hello Style!</h1>
+                <h1 style= {{backgroundColor : "#76FF03"}}>Hello Style!</h1>
                 <p>Adding a title style !</p>
             </div>
         )
     }
 }
 ReactDOM.render(<MyHeader />, document.getElementById('root'));
-
-// Note: In JSX, JavaScript expressions are written inside curly braces, and since JavaScript
-// objects also use curly braces, the styling in the example above is written inside two sets
-// of curly braces {{}}.
